94df9524b395e637cc50cdd91e822d767d0954be
Alireza <26859725+tralireza@users.noreply.github.com>
2023-01-08 12:08:08 +0000

    1544 Make String Great

--- LC/basic_test.go
+++ LC/basic_test.go
@@ -29,3 +29,24 @@ func Test1614(t *testing.T) {
 	log.Print("3 ?= ", maxDepth("(1+(2*3)+((8)/4))+1"))
 	log.Print("3 ?= ", maxDepth("(1)+((2))+(((3)))"))
 }
+
+// 1544 Make String Great
+func Test1544(t *testing.T) {
+	makeGood := func(s string) string {
+		i := 0
+		for i < len(s)-1 {
+			log.Printf("%q %[1]d %q %[2]d | %d", s[i], s[i+1], s[i]-s[i+1])
+
+			if s[i]-s[i+1] == 'a'-'A' || s[i]-s[i+1] == 256-('a'-'A') {
+				s = s[:i] + s[i+2:]
+				i = 0
+			} else {
+				i++
+			}
+		}
+		return s
+	}
+
+	log.Print(" ?= ", makeGood("abBACc"))
+	log.Print(" ?= ", makeGood("Pp"))
+}
