59bb15fff18d39f008c80bfd2815f083f20ed1b8
Alireza <26859725+tralireza@users.noreply.github.com>
2023-01-20 11:29:25 +0000

    BFS & DFS Demo: passing dequeue function to Search for both DFS/BFS

--- demo/BFS/demo_test.go
+++ demo/BFS/demo_test.go
@@ -9,10 +9,10 @@ func init() {
 	log.Print("> BFS Demo")
 }
 
-func TestDemo(t *testing.T) {
+func TestGrid(t *testing.T) {
 	d := NewDemo(10, 56)
-	d.AddBlock(64)
-	d.AddDoor(8)
+	d.AddBlock(128)
+	d.AddDoor(16)
 	d.Draw()
 }
 
@@ -26,6 +26,13 @@ func TestBFS(t *testing.T) {
 func TestDFS(t *testing.T) {
 	d := NewDemo(10, 56)
 	d.AddBlock(128)
+	d.AddDoor(16)
+	d.DFS(Point{5, 28})
+}
+
+func TestLargeGrid(t *testing.T) {
+	d := NewDemo(40, 56)
+	d.AddBlock(512)
 	d.AddDoor(16)
 	d.DFS(Point{5, 28})
 }
