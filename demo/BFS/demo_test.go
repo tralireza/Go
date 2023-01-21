8182e1595edd8091156a06ade3cc51db1814ac2d
Alireza <26859725+tralireza@users.noreply.github.com>
2023-01-21 12:11:22 +0000

    BFS & DFS Demo: terminal escapes for clear hide/show cursor

--- demo/BFS/demo_test.go
+++ demo/BFS/demo_test.go
@@ -1,8 +1,10 @@
 package dBFS
 
 import (
+	"fmt"
 	"log"
 	"testing"
+	"time"
 )
 
 func init() {
@@ -10,16 +12,24 @@ func init() {
 }
 
 func TestGrid(t *testing.T) {
+	fmt.Printf("\033[2J") // cls: clear screen
+
 	d := NewDemo(10, 56)
 	d.AddBlock(128)
 	d.AddDoor(16)
 	d.Draw()
+
+	fmt.Printf("\x1b[?25l") // low(hide) cursor
+	time.Sleep(time.Second)
+	fmt.Printf("\x1b[?25h") // high(show) cursor
+	fmt.Printf("\n")
 }
 
 func TestBFS(t *testing.T) {
 	d := NewDemo(10, 56)
 	d.AddBlock(128)
 	d.AddDoor(16)
+
 	d.BFS(Point{5, 28})
 }
 
@@ -27,6 +37,7 @@ func TestDFS(t *testing.T) {
 	d := NewDemo(10, 56)
 	d.AddBlock(128)
 	d.AddDoor(16)
+
 	d.DFS(Point{5, 28})
 }
 
