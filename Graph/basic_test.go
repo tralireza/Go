d3f95cffac15bc105a41d1606690fb0562d7edbf
Alireza <26859725+tralireza@users.noreply.github.com>
2023-03-01 17:36:01 +0000

    Graph: un/directed graph adjacency list & matrix

--- Graph/basic_test.go
+++ Graph/basic_test.go
@@ -10,6 +10,8 @@ func init() {
 }
 
 func TestSetup(t *testing.T) {
-	g := New(7, [][2]byte{{0, 1}, {2, 3}, {1, 3}, {4, 5}, {5, 6}, {3, 6}})
-	log.Print(g)
+	dg := GDirected(7, [][2]byte{{0, 1}, {2, 3}, {1, 3}, {4, 5}, {5, 6}, {3, 6}})
+	log.Print(dg)
+	ug := GUndirected(7, [][2]byte{{0, 1}, {2, 3}, {1, 3}, {4, 5}, {5, 6}, {3, 6}})
+	log.Print(ug)
 }
