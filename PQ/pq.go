e406ac9ae896e6872b1a2f50b3fa528251c8f04d
Alireza <26859725+tralireza@users.noreply.github.com>
2023-06-11 12:38:50 +0100

    2462: PQ: TotalCost

---
+++ PQ/pq.go
@@ -0,0 +1,7 @@
+package lpq
+
+func TotalCost(costs []int, k int, candidates int) int64 {
+	tcost := int64(0)
+
+	return tcost
+}
---
+++ PQ/pq_test.go
@@ -0,0 +1,6 @@
+package lpq
+
+import "testing"
+
+func Test2462(t *testing.T) {
+}
