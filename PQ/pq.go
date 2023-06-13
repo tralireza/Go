930568bad6ba97c846280f22840f0dc6ef5f7956
Alireza <26859725+tralireza@users.noreply.github.com>
2023-06-13 20:31:54 +0100

    2462: Heap: TotalCost

--- PQ/pq.go
+++ PQ/pq.go
@@ -1,7 +1,69 @@
 package lpq
 
+import (
+	"container/heap"
+	"log"
+	"sort"
+)
+
+type IHeap struct{ sort.IntSlice }
+
+func (h *IHeap) Push(v any) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *IHeap) Pop() any {
+	v := h.IntSlice[len(h.IntSlice)-1]
+	h.IntSlice = h.IntSlice[:len(h.IntSlice)-1]
+	return v
+}
+
 func TotalCost(costs []int, k int, candidates int) int64 {
+	L, R := IHeap{}, IHeap{}
+	l, r := 0, len(costs)-1
+	for ; l < candidates; l++ {
+		heap.Push(&L, costs[l])
+	}
+	for ; r >= max(len(costs)-candidates, candidates); r-- {
+		heap.Push(&R, costs[r])
+	}
+	log.Printf("(%d,%d) %v %v", l, r, L, R)
+
 	tcost := int64(0)
+	for ; k > 0 && L.Len() > 0 && R.Len() > 0; k-- {
+		c := min(L.IntSlice[0], R.IntSlice[0])
+		tcost += int64(c)
+		if L.IntSlice[0] <= R.IntSlice[0] {
+			heap.Pop(&L)
+			if l <= r {
+				heap.Push(&L, costs[l])
+				l++
+			}
+		} else {
+			heap.Pop(&R)
+			if l <= r {
+				heap.Push(&R, costs[r])
+				r--
+			}
+		}
+
+		log.Printf("(%d,%d) %v %v", l, r, L, R)
+	}
+
+	for ; k > 0; k-- {
+		if L.Len() > 0 {
+			tcost += int64(heap.Pop(&L).(int))
+			if l <= r {
+				heap.Push(&L, costs[l])
+				l++
+			}
+		} else {
+			tcost += int64(heap.Pop(&R).(int))
+			if l <= r {
+				heap.Push(&R, costs[r])
+				r--
+			}
+		}
+
+		log.Printf("(%d,%d) %v %v", l, r, L, R)
+	}
 
 	return tcost
 }
--- PQ/pq_test.go
+++ PQ/pq_test.go
@@ -1,6 +1,15 @@
 package lpq
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
+
+func init() {
+	log.SetFlags(0)
+}
 
 func Test2462(t *testing.T) {
+	log.Print(TotalCost([]int{17, 12, 10, 2, 7, 2, 11, 20, 8}, 9, 4))
+	log.Print(TotalCost([]int{1, 2, 4, 1}, 3, 3))
 }
