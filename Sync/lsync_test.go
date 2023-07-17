5b65f6b041f745a12ef3c5b4184815b7c9f42dc4
Alireza <26859725+tralireza@users.noreply.github.com>
2023-07-17 14:44:42 +0100

    FanIn Demuxing: tidyup

--- Sync/lsync_test.go
+++ Sync/lsync_test.go
@@ -9,19 +9,38 @@ import (
 	"time"
 )
 
-func TestFanIn(t *testing.T) {
-	Squarer := func(inc <-chan int) <-chan int {
-		c := make(chan int)
-		go func() {
-			defer close(c)
-			for i := range inc {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(175)))
-				c <- i * i
-			}
-		}()
-		return c
-	}
+func Squarer(inc <-chan int) <-chan int {
+	c := make(chan int)
+	go func() {
+		defer close(c)
+		for i := range inc {
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(250)))
+			c <- i * i
+		}
+	}()
+	return c
+}
 
+func FanIn(cs ...<-chan int) <-chan int {
+	outc := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go func(c <-chan int) {
+			defer wg.Done()
+			for i := range c {
+				outc <- i
+			}
+		}(c)
+	}
+	go func() {
+		defer close(outc)
+		wg.Wait()
+	}()
+	return outc
+}
+
+func TestFanIn(t *testing.T) {
 	c := make(chan int)
 	go func() {
 		defer close(c)
@@ -30,25 +49,6 @@ func TestFanIn(t *testing.T) {
 			c <- i
 		}
 	}()
-
-	FanIn := func(cs ...<-chan int) <-chan int {
-		outc := make(chan int)
-		wg := sync.WaitGroup{}
-		wg.Add(len(cs))
-		for _, c := range cs {
-			go func(c <-chan int) {
-				defer wg.Done()
-				for i := range c {
-					outc <- i
-				}
-			}(c)
-		}
-		go func() {
-			defer close(outc)
-			wg.Wait()
-		}()
-		return outc
-	}
 
 	ts := time.Now()
 	for i := range FanIn(Squarer(c), Squarer(c), Squarer(c)) {
