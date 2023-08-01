dfff021b11e99572fd5c9e03b389e8ecc8a78cd1
Alireza <26859725+tralireza@users.noreply.github.com>
2023-08-01 15:08:58 +0100

    reflect.TypeOf: interface{} Elem() to get value

--- Sync/reflect_test.go
+++ Sync/reflect_test.go
@@ -7,8 +7,10 @@ import (
 )
 
 func TestReflect(t *testing.T) {
-	i := 42
-	v, p := reflect.ValueOf(i), reflect.ValueOf(&i)
-	log.Printf("%T %[1]v %v  %T %[3]v %v", v, v.Kind(), p, p.Kind())
+	n := 42
+	v, p := reflect.ValueOf(n), reflect.ValueOf(&n)
+	log.Printf("%T %[1]v %v | %T %[3]v %v", v, v.Kind(), p, p.Kind())
 
+	var i interface{} = new(int)
+	log.Printf("interface{}: %v %v %v", reflect.TypeOf(i), reflect.TypeOf(i).Kind(), reflect.ValueOf(i).Elem())
 }
