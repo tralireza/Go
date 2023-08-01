0311085514d3d915b14dc201da3c76c5d61969fe
Alireza <26859725+tralireza@users.noreply.github.com>
2023-08-01 14:36:25 +0100

    reflect.ValueOf: Kind()

--- Sync/reflect_test.go
+++ Sync/reflect_test.go
@@ -9,5 +9,6 @@ import (
 func TestReflect(t *testing.T) {
 	i := 42
 	v, p := reflect.ValueOf(i), reflect.ValueOf(&i)
-	log.Printf("%T %[1]v  %T %[2]v", v, p)
+	log.Printf("%T %[1]v %v  %T %[3]v %v", v, v.Kind(), p, p.Kind())
+
 }
