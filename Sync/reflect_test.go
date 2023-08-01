7c95463031b3eee24a90b8933f3afee01da3f141
Alireza <26859725+tralireza@users.noreply.github.com>
2023-08-01 15:16:02 +0100

    reflect.TypeOf: interface{} of struct{a,b int} Elem() to get Zero value of

--- Sync/reflect_test.go
+++ Sync/reflect_test.go
@@ -11,6 +11,10 @@ func TestReflect(t *testing.T) {
 	v, p := reflect.ValueOf(n), reflect.ValueOf(&n)
 	log.Printf("%T %[1]v %v | %T %[3]v %v", v, v.Kind(), p, p.Kind())
 
-	var i interface{} = new(int)
-	log.Printf("interface{}: %v %v %v", reflect.TypeOf(i), reflect.TypeOf(i).Kind(), reflect.ValueOf(i).Elem())
+	var i interface{}
+	i = new(int)
+	log.Printf("interface{} = new(int): %v, %v, %v", reflect.TypeOf(i), reflect.TypeOf(i).Kind(), reflect.ValueOf(i).Elem())
+
+	i = new(struct{ a, b int })
+	log.Printf("interface{} = new(struct{}): %v, %v, %v", reflect.TypeOf(i), reflect.TypeOf(i).Kind(), reflect.ValueOf(i).Elem())
 }
