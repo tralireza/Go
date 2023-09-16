626b69b8ebca7afc0c682e1342cc732bea67fff3
Alireza <26859725+tralireza@users.noreply.github.com>
2023-09-16 16:19:00 +0100

    DP 714 Best Time to Buy&Sell with Fee: base frame (improved)

--- LC/dp_test.go
+++ LC/dp_test.go
@@ -12,8 +12,13 @@ func init() {
 // 714m Best Time to Buy & Sell with Fee
 func Test714(t *testing.T) {
 	maxProfit := func(prices []int, fee int) int {
+		// [day][transactions][action]
+		profit := make([][2][2]int, len(prices))
 
-		return 0
+		log.Print("-> ", profit)
+
+		// last day [n-1]-- of using transaction allowed -[1]- with no stock left (ie sell) --[0]
+		return profit[len(prices)-1][1][0]
 	}
 
 	log.Print("8 ?= ", maxProfit([]int{1, 3, 2, 8, 4, 9}, 2))
