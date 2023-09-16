c1f85fc500c28514120400469c3fb264508e6d0d
Alireza <26859725+tralireza@users.noreply.github.com>
2023-09-16 13:07:00 +0100

    DP 714 Best Time to Buy&Sell with Fee: base frame

---
+++ LC/dp_test.go
@@ -0,0 +1,20 @@
+package lc
+
+import (
+	"log"
+	"testing"
+)
+
+func init() {
+	log.Print("> DP (mXdim)")
+}
+
+// 714m Best Time to Buy & Sell with Fee
+func Test714(t *testing.T) {
+	maxProfit := func(prices []int, fee int) int {
+
+		return 0
+	}
+
+	log.Print("8 ?= ", maxProfit([]int{1, 3, 2, 8, 4, 9}, 2))
+}
