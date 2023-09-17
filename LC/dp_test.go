9aac14bc19abf0085354db586a9e9370539183a1
Alireza <26859725+tralireza@users.noreply.github.com>
2023-09-17 17:05:00 +0100

    DP 121 Best Time to Buy&Sell Kadane's algorithm -> maximum subarray sum

--- LC/dp_test.go
+++ LC/dp_test.go
@@ -2,6 +2,7 @@ package lc
 
 import (
 	"log"
+	"math"
 	"testing"
 )
 
@@ -15,11 +16,37 @@ func Test714(t *testing.T) {
 		// [day][transactions][action]
 		profit := make([][2][2]int, len(prices))
 
-		log.Print("-> ", profit)
+		profit[0][0][1] = math.MinInt
+
+		for i := 1; i < len(prices); i++ {
+			log.Print("-> ", profit)
+		}
 
 		// last day [n-1]-- of using transaction allowed -[1]- with no stock left (ie sell) --[0]
 		return profit[len(prices)-1][1][0]
 	}
 
 	log.Print("8 ?= ", maxProfit([]int{1, 3, 2, 8, 4, 9}, 2))
+}
+
+// 121e Best Time to Buy & Sell: Kadane's algorithm
+func Test121(t *testing.T) {
+	maxProfix := func(prices []int) int {
+		// Kadane's
+		best := 0 // -Infinity
+		cur := 0
+		for i := 0; i < len(prices); i++ {
+			diff := 0
+			if i > 0 {
+				diff = prices[i] - prices[i-1]
+			}
+
+			cur = max(diff, cur+diff)
+			best = max(best, cur)
+		}
+		return best
+	}
+
+	log.Print("5 ?= ", maxProfix([]int{7, 1, 5, 3, 6, 4}))
+	log.Print("0 ?= ", maxProfix([]int{7, 6, 4, 3, 1}))
 }
