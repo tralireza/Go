dc73d8c3a5c5e5d23244541305322b4460ad19ec
Alireza <26859725+tralireza@users.noreply.github.com>
2023-10-12 22:16:00 +0100

    Trie: insert

---
+++ LC/trie_test.go
@@ -0,0 +1,59 @@
+package lc
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+)
+
+func init() {
+	log.Print("> Trie")
+}
+
+type Trie struct {
+	Children [26]*Trie
+	IsNode   bool
+}
+
+func (o Trie) String() string {
+	sb := strings.Builder{}
+	for i := byte(0); i < 26; i++ {
+		c := byte('-')
+		if o.Children[i] != nil {
+			c = 'a' + i
+		}
+		sb.WriteByte(c)
+	}
+	c := ':'
+	if o.IsNode {
+		c = '+'
+	}
+	return fmt.Sprintf("[%s %c]", sb.String(), c)
+}
+
+func NewTrie() *Trie { return &Trie{} }
+
+func (o *Trie) Insert(word string) {
+	n := o
+	for i := 0; i < len(word); i++ {
+		c := word[i]
+		if n.Children[c-'a'] == nil {
+			n.Children[c-'a'] = &Trie{}
+		}
+		n = n.Children[c-'a']
+	}
+	n.IsNode = true
+}
+
+func TestTrie(t *testing.T) {
+	T := NewTrie()
+	T.Insert("trie")
+	T.Insert("is")
+	T.Insert("prefix")
+	T.Insert("tree")
+
+	log.Print(T)
+	log.Print(T.Children['i'-'a'])
+	log.Print(T.Children['i'-'a'].Children['s'-'a'])
+}
