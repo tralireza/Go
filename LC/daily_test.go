da2fa4fdf6d622615a968733fccba37ba75c1a7a
Alireza <26859725+tralireza@users.noreply.github.com>
2023-11-16 21:11:05 +0000

    BinSearch 287m Find Duplicate Number: left right are 1 and n for BinSearch

--- LC/daily_test.go
+++ LC/daily_test.go
@@ -374,11 +374,42 @@ func Test143(t *testing.T) {
 
 // 287m Find Duplicate Number
 func Test287(t *testing.T) {
+	// BinSearch
 	findDuplicate := func(nums []int) int {
+		countLess := func(v int) bool {
+			f := 0
+			for _, n := range nums {
+				if n <= v {
+					f++
+				}
+			}
+			return f > v
+		}
+
+		l, r := 1, len(nums)-1
+		for l < r {
+			m := l + (r-l)>>1
+			if countLess(m) {
+				r = m
+			} else {
+				l = m + 1
+			}
+		}
+
+		return l
+	}
+
+	// Two Pointers
+	f1 := func(nums []int) int {
 		i, d := 0, 1
 		for nums[i] != nums[d] {
 			d += 2
 			i += 1
+
+			if i == d {
+				d += 1
+			}
+
 			if d >= len(nums) {
 				d %= len(nums)
 			}
@@ -390,4 +421,7 @@ func Test287(t *testing.T) {
 	}
 
 	log.Print("2 ?= ", findDuplicate([]int{1, 3, 4, 2, 2}))
+	log.Print("1 ?= ", findDuplicate([]int{1, 3, 4, 2, 1}))
+	log.Print("4 ?= ", findDuplicate([]int{4, 3, 1, 4, 2}))
+	log.Print("1 ?= ", f1([]int{1, 1}))
 }
