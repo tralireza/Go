b286c73a102b7d23ae33d56222bbdee4d4894e57
Alireza <26859725+tralireza@users.noreply.github.com>
2023-11-23 21:53:54 +0000

    1091m Shorted Path in Ninary Matrix: DFS with O(1) space using grid as Visited store

--- LC/search_test.go
+++ LC/search_test.go
@@ -1,6 +1,7 @@
 package lc
 
 import (
+	"fmt"
 	"log"
 	"testing"
 )
@@ -12,10 +13,46 @@ func init() {
 // 1091m Shortest Path in Binary Matrix
 func Test1091(t *testing.T) {
 	shortestPathBinaryMatrix := func(grid [][]int) int {
-		return 0
+		type P struct{ i, j int }
+
+		Q := []P{}
+		if grid[0][0] == 0 {
+			Q = append(Q, P{0, 0})
+			grid[0][0] = 1
+		}
+
+		m, n := len(grid), len(grid[0])
+
+		var v P
+		for len(Q) > 0 {
+			v, Q = Q[0], Q[1:]
+			if v.i == m-1 && v.j == n-1 {
+				return grid[m-1][n-1]
+			}
+
+			for _, d := range [8]P{{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, -1}, {1, 1}, {-1, 1}, {-1, -1}} {
+				u := P{v.i + d.i, v.j + d.j}
+
+				if u.i >= 0 && u.j >= 0 && m > u.i && n > u.j && grid[u.i][u.j] == 0 {
+					grid[u.i][u.j] = 1 + grid[v.i][v.j]
+
+					Q = append(Q, u)
+				}
+			}
+		}
+
+		return -1
 	}
 
 	gridSet := func() [][]int { return [][]int{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}} }
+
+	grid := gridSet()
+	for i := range grid {
+		for _, v := range grid[i] {
+			fmt.Printf("| %d ", v)
+		}
+		fmt.Println("|")
+	}
 
 	log.Print("4 ?= ", shortestPathBinaryMatrix(gridSet()))
 }
