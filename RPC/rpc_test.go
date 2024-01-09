7b3304714b9861426762351476ff3fdb85db1a3d
Alireza <26859725+tralireza@users.noreply.github.com>
2024-01-09 00:08:15 +0000

    DP: Knapsack

--- RPC/rpc_test.go
+++ RPC/rpc_test.go
@@ -1,6 +1,8 @@
 package lrcp
 
 import (
+	"fmt"
+	"log"
 	"testing"
 	"time"
 )
@@ -16,5 +18,45 @@ func TestCall(t *testing.T) {
 	}
 	for i := 0; i < 15; i++ {
 		<-errc
+	}
+}
+
+func Knapsack(ksCapacity int) int {
+	type Fruit struct {
+		Size, Price int
+		Name        string
+	}
+
+	fruits := []Fruit{{4, 4500, "Plum"},
+		{5, 5700, "Apple"},
+		{2, 2250, "Orange"},
+		{1, 1100, "Strawberry"},
+		{6, 6700, "Melon"}}
+
+	items := make([]int, ksCapacity+1)
+	values := make([]int, ksCapacity+1)
+
+	for i := 0; i < len(fruits); i++ {
+		for j := fruits[i].Size; j <= ksCapacity; j++ {
+			left := j - fruits[i].Size
+			newValue := values[left] + fruits[i].Price
+			if newValue > values[j] {
+				values[j] = newValue
+				items[j] = i
+			}
+		}
+	}
+
+	for j := ksCapacity; j > 0; {
+		fmt.Printf("%v, ", fruits[items[j]].Name)
+		j -= fruits[items[j]].Size
+	}
+	fmt.Println()
+	return values[ksCapacity]
+}
+
+func TestKnapsack(t *testing.T) {
+	for _, v := range []int{8, 13} {
+		log.Printf("+ %d: %d", v, Knapsack(v))
 	}
 }
