6d8acb045cf3c8dd7c2941cf6adb460ec4fd2c30
Alireza <26859725+tralireza@users.noreply.github.com>
2024-01-20 20:05:42 +0000

    TermColor: ternimal colorful writes: Red test

--- TermColor/color_test.go
+++ TermColor/color_test.go
@@ -1,12 +1,21 @@
 package termcolor
 
 import (
+	"bytes"
 	"log"
 	"testing"
 )
 
 func init() {
 	log.Print("TermColor >")
+}
+
+func TestOut(t *testing.T) {
+	var bfr bytes.Buffer
+	Red.Fprintf(&bfr, "%d", Red)
+	if !bytes.Equal(bfr.Bytes(), []byte{27, 91, 51, 49, 109, 51, 49, 27, 91, 48, 109}) {
+		t.Fatalf(`Wrong escape! "\x1b[31m31\x1b[0m" != %q`, bfr.String())
+	}
 }
 
 func TestColor(t *testing.T) {
