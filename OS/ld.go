c9a01c065fafdb47fdd6742e7aa2623c1e7341a2
Alireza <26859725+tralireza@users.noreply.github.com>
2024-01-20 23:16:34 +0000

    Daemon

---
+++ OS/ld.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
+func main() {
+	pid := os.Getpid()
+	defer log.Printf("[%d] done.", pid)
+	log.Printf("[%d] start...", pid)
+	log.Printf("[%d] ppid: %d", pid, os.Getppid())
+
+	if len(os.Args) != 1 {
+		log.Print(os.Args)
+		for {
+			log.Printf("[%d] daemon: %v", pid, time.Now())
+			time.Sleep(5 * time.Second)
+		}
+		return
+	}
+
+	cwd, _ := os.Getwd()
+	log.Printf("[%d] -> %v", pid, filepath.Join(cwd, os.Args[0]))
+
+	cmd := exec.Command(filepath.Join(cwd, os.Args[0]), "--daemon")
+	cmd.Stdout, cmd.Stdin, cmd.Dir = os.Stdout, os.Stdin, "/"
+	log.Print(cmd.Args)
+	if err := cmd.Start(); err != nil {
+		log.Printf("[%d] <FORK> %v", pid, err)
+		return
+	}
+
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		log.Printf("[%d] <RELEASE> %v", pid, err)
+		return
+	}
+	p.Release()
+}
