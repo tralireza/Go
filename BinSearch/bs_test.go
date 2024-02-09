1ae72a7a7cb84db9e54a9e70b15e602a6a7edf2c
Alireza <26859725+tralireza@users.noreply.github.com>
2024-02-09 21:34:21 +0000

    2300: BinSearch: SuccessfulPairs test cases

--- BinSearch/bs_test.go
+++ BinSearch/bs_test.go
@@ -7,4 +7,5 @@ import (
 
 func Test2300(t *testing.T) {
 	log.Print(SuccessfulPairs([]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, int64(7)))
+	log.Print(SuccessfulPairs([]int{3, 1, 2}, []int{8, 5, 8}, int64(16)))
 }
