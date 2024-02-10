cf86e313511057bc7e610de85235ba179535ede5
Alireza <26859725+tralireza@users.noreply.github.com>
2024-02-10 12:02:11 +0000

    2300: BinSearch: left most BinSearch with duplicates

--- BinSearch/bs.go
+++ BinSearch/bs.go
@@ -1,6 +1,9 @@
 package bsearch
 
-import "log"
+import (
+	"log"
+	"slices"
+)
 
 func init() {
 	log.SetFlags(0)
@@ -9,6 +12,36 @@ func init() {
 
 // 2300
 func SuccessfulPairs(spells []int, potions []int, success int64) []int {
+	leftBSearch := func(Sdup []int, x int) int {
+		l, r := 0, len(Sdup)-1
+		for l < r {
+			m := (l + r) / 2
+			if Sdup[m] < x {
+				l = m + 1
+			} else {
+				r = m
+			}
+		}
+		return l
+	}
+
+	slices.Sort(potions)
+	log.Print(potions)
+
 	pairs := make([]int, len(spells))
+	for i, v := range spells {
+		x := (success + int64(v) - 1) / int64(v)
+		if x > 1 {
+			l := leftBSearch(potions, int(x))
+			pairs[i] = len(potions) - l
+			if l == len(potions)-1 && int64(potions[l]) < x {
+				log.Printf("%d: %d %d", l, potions[l], x)
+
+				pairs[i]--
+			}
+		} else {
+			pairs[i] = len(potions)
+		}
+	}
 	return pairs
 }
--- BinSearch/bs_test.go
+++ BinSearch/bs_test.go
@@ -2,10 +2,19 @@ package bsearch
 
 import (
 	"log"
+	"slices"
 	"testing"
 )
 
 func Test2300(t *testing.T) {
-	log.Print(SuccessfulPairs([]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, int64(7)))
-	log.Print(SuccessfulPairs([]int{3, 1, 2}, []int{8, 5, 8}, int64(16)))
+	for _, v := range [][][]int{
+		{[]int{5, 1, 3}, []int{1, 2, 3, 4, 5}, []int{7}, []int{4, 0, 3}},
+		{[]int{3, 1, 2, 16}, []int{8, 5, 8}, []int{16}, []int{2, 0, 2, 3}}} {
+
+		spells, potions, success := v[0], v[1], int64(v[2][0])
+		if pairs := SuccessfulPairs(spells, potions, success); slices.Compare(pairs, v[3]) != 0 {
+			t.Fatalf("Wrong pairs! %v != %v", pairs, v[3])
+		}
+		log.Printf("+ %v %v  -%d->  %v", spells, potions, success, v[3])
+	}
 }
