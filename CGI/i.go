54a1cf95259d3e9d2f24020260a9b95e76de088a
Alireza <26859725+tralireza@users.noreply.github.com>
2024-03-02 09:38:25 +0000

    whitespaces

--- CGI/i.go
+++ CGI/i.go
@@ -7,16 +7,13 @@ import (
 )
 
 func main() {
-	fmt.Printf("content-type: text/html\n\n")
+	fmt.Printf("Content-Type: text/html\n\n")
 	fmt.Printf("<h4>%s</h4>\n", os.Getenv("REMOTE_ADDR"))
 
-	fmt.Println("<pre>")
-
-	fmt.Println()
+	fmt.Println("<pre>\n")
 	for i, s := range os.Environ() {
 		v := strings.SplitN(s, "=", 2)
 		fmt.Printf("%2d %s %s\n", i+1, s, v)
 	}
-
 	fmt.Println("</pre>")
 }
