7bb40dd262e9548c133c9ab01126367da7d3c1eb
Alireza <26859725+tralireza@users.noreply.github.com>
2024-03-04 20:27:15 +0000

    2542: PQ/Heap MaxScore

--- Heap/lheap.go
+++ Heap/lheap.go
@@ -1,14 +1,45 @@
 package lheap
 
 import (
+	"container/heap"
 	"log"
 	"slices"
+	"sort"
 )
 
-// 2542
-func MaxScore(nums1, nums2 []int, k int) int {
+type IHeap struct{ sort.IntSlice }
 
-	return 0
+func (o *IHeap) Push(v any) { o.IntSlice = append(o.IntSlice, v.(int)) }
+func (o *IHeap) Pop() any {
+	v := (o.IntSlice)[len(o.IntSlice)-1]
+	o.IntSlice = o.IntSlice[:len(o.IntSlice)-1]
+	return v
+}
+
+// 2542
+func MaxScore(nums1, nums2 []int, k int) int64 {
+	type E struct{ s, m int }
+	L := []E{}
+	for i := 0; i < len(nums1); i++ {
+		L = append(L, E{s: nums1[i], m: nums2[i]})
+	}
+	slices.SortFunc(L, func(a, b E) int { return b.m - a.m })
+	log.Print(L)
+
+	H := IHeap{}
+	score, lSum := int64(0), int64(0)
+	for _, l := range L {
+		heap.Push(&H, l.s)
+		lSum += int64(l.s)
+
+		if H.Len() > k {
+			lSum -= int64(heap.Pop(&H).(int))
+		}
+		if H.Len() == k {
+			score = max(score, lSum*int64(l.m))
+		}
+	}
+	return score
 }
 
 // 948
--- Heap/lheap_test.go
+++ Heap/lheap_test.go
@@ -6,11 +6,14 @@ import (
 )
 
 func Test2542(t *testing.T) {
-	if MaxScore(
+	if r := MaxScore(
 		[]int{1, 3, 3, 2},
-		[]int{2, 1, 4, 3}, 3) != 12 {
-		t.Fatalf("Wrong maxScore! must be: %d", 12)
+		[]int{2, 1, 4, 3}, 3); r != 12 {
+		t.Fatalf("Wrong maxScore! must be: %d != %d", 12, r)
 	}
+
+	log.Print(MaxScore([]int{1, 4}, []int{3, 1}, 2))
+	log.Print(MaxScore([]int{2, 1, 14, 12}, []int{11, 7, 13, 6}, 3))
 }
 
 func Test948(t *testing.T) {
@@ -18,7 +21,11 @@ func Test948(t *testing.T) {
 		log.Fatal("Wrong score! must be: 2")
 	}
 
-	for _, x := range [][][]int{{{200, 100}, {150, 1}}, {{100}, {50, 0}}, {{81, 91, 31}, {73, 1}}, {{25}, {150, 1}}} {
+	for _, x := range [][][]int{{
+		{200, 100}, {150, 1}},
+		{{100}, {50, 0}},
+		{{81, 91, 31}, {73, 1}},
+		{{25}, {150, 1}}} {
 		log.Printf("+ Score: %d   Power: %d   Tokens: %v", x[1][1], x[1][0], x[0])
 		if r := BagOfTokensScore(x[0], x[1][0]); r != x[1][1] {
 			t.Errorf("Wrong score! must be: %d != %d", x[1][1], r)
