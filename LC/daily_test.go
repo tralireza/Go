6e2318d53c2c6387c132144454cabb3be04f4886
Alireza <26859725+tralireza@users.noreply.github.com>
2024-03-14 14:16:07 +0000

    PrefixSum 930 Binary Subarrays With Sum base implementaiton with PrefixSum & HashMap

--- LC/daily_test.go
+++ LC/daily_test.go
@@ -10,9 +10,28 @@ func init() {}
 // 930m
 func Test930(t *testing.T) {
 	countSubarraysWithSum := func(nums []int, goal int) int {
+		frq := map[int]int{}
 
-		return 0
+		x, pfxSum := 0, 0
+		for _, n := range nums {
+			pfxSum += n
+			if pfxSum == goal {
+				x++
+			}
+
+			if f, ok := frq[pfxSum-goal]; ok {
+				x += f
+			}
+
+			frq[pfxSum]++
+
+			log.Print(frq)
+		}
+
+		return x
 	}
 
 	log.Print("4 ?= ", countSubarraysWithSum([]int{1, 0, 1, 0, 1}, 2))
+	log.Print("8 ?= ", countSubarraysWithSum([]int{1, 0, 1, 0, 1}, 1))
+	log.Print("15 ?= ", countSubarraysWithSum([]int{0, 0, 0, 0, 0}, 0))
 }
