e6c9a3e8d077cce9d7d6cb1b41deea3e7d9062b0
Alireza <26859725+tralireza@users.noreply.github.com>
2024-03-14 17:32:56 +0000

    PrefixSum 930 Binary Subarrays With Sum base implementaiton SlidingWindow one pass O(1) space

--- LC/daily_test.go
+++ LC/daily_test.go
@@ -9,6 +9,7 @@ func init() {}
 
 // 930m
 func Test930(t *testing.T) {
+	// PrefixSum & HashMap
 	countSubarraysWithSum := func(nums []int, goal int) int {
 		frq := map[int]int{}
 
@@ -18,14 +19,36 @@ func Test930(t *testing.T) {
 			if pfxSum == goal {
 				x++
 			}
-
 			if f, ok := frq[pfxSum-goal]; ok {
 				x += f
 			}
-
 			frq[pfxSum]++
+		}
 
-			log.Print(frq)
+		return x
+	}
+
+	// SlidingWindow -> space: O(1)
+	countSubarraysWithSum2 := func(nums []int, goal int) int {
+		x := 0
+
+		l, csum := 0, 0
+		leadingZeros := 0
+		for r, n := range nums {
+			csum += n
+
+			for ; l < r && nums[l] == 0 || csum > goal; l++ {
+				if nums[l] == 0 {
+					leadingZeros++
+				} else {
+					leadingZeros = 0
+				}
+				csum -= nums[l]
+			}
+
+			if csum == goal {
+				x += 1 + leadingZeros
+			}
 		}
 
 		return x
@@ -34,4 +57,6 @@ func Test930(t *testing.T) {
 	log.Print("4 ?= ", countSubarraysWithSum([]int{1, 0, 1, 0, 1}, 2))
 	log.Print("8 ?= ", countSubarraysWithSum([]int{1, 0, 1, 0, 1}, 1))
 	log.Print("15 ?= ", countSubarraysWithSum([]int{0, 0, 0, 0, 0}, 0))
+
+	log.Print("4 ?= ", countSubarraysWithSum2([]int{1, 0, 1, 0, 1}, 2))
 }
