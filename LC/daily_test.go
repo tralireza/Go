2d78b0e8a68f859abf2d64c478d0897ec7b15ef3
Alireza <26859725+tralireza@users.noreply.github.com>
2024-03-14 12:19:28 +0000

    PrefixSum 930 Binary Subarrays With Sum base frame

---
+++ LC/daily_test.go
@@ -0,0 +1,18 @@
+package lc
+
+import (
+	"log"
+	"testing"
+)
+
+func init() {}
+
+// 930m
+func Test930(t *testing.T) {
+	countSubarraysWithSum := func(nums []int, goal int) int {
+
+		return 0
+	}
+
+	log.Print("4 ?= ", countSubarraysWithSum([]int{1, 0, 1, 0, 1}, 2))
+}
